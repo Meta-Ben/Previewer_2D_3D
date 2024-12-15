cf21f4f8d90a7e43ca695cd5e53c5e4f997637ae
Meta-Ben <metautbenjamin@hotmail.fr>
2024-12-15 17:15:00 +0100

    Rename Previewer2D3D folder and variables into DualEditor

--- addons/DualEditor/DualEditor.gd
+++ addons/DualEditor/DualEditor.gd
@@ -1,5 +1,5 @@
 @tool
-class_name Previewer2D3D
+class_name DualEditor
 extends Control
 
 var editor_plugin : EditorPlugin
@@ -9,8 +9,9 @@ var editor_plugin : EditorPlugin
 @onready var current_subviewport : SubViewport = $Current_SubViewport
 @onready var subviewport_2D : SubViewport = $SubViewport_2D
 @onready var subviewport_3D : SubViewport = $SubViewport_3D
+
 #Values
-var current_preview_mode : int
+var current_dual_editor_mode : int
 var godot_split_size : float = 4.0 #Pixel width of Godot's split container handlebar (prevents 2D viewer from pushing 3D viewer back)
 
 # Called when the node enters the scene tree for the first time.
@@ -19,27 +20,27 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	#Force viewport size update
-	match current_preview_mode:
-		Previewer2D3D_Enums.PreviewMode.PREVIEW_2D:
+	match current_dual_editor_mode:
+		DualEditor_Enum.DualEditorMode.MODE_2D:
 			subviewport_2D.size = size
-		Previewer2D3D_Enums.PreviewMode.PREVIEW_3D:
+		DualEditor_Enum.DualEditorMode.MODE_3D:
 			subviewport_3D.size = size
 
-func switch_preview(preview_mode : int, preview_size : Vector2) -> void:
-	current_preview_mode = preview_mode
-	match current_preview_mode:
-		Previewer2D3D_Enums.PreviewMode.PREVIEW_2D:
+func switch_mode(dual_editor_mode : int, editor_size : Vector2) -> void:
+	current_dual_editor_mode = dual_editor_mode
+	match current_dual_editor_mode:
+		DualEditor_Enum.DualEditorMode.MODE_2D:
 			current_subviewport = subviewport_2D
 			subviewport_2D.world_2d = EditorInterface.get_editor_viewport_2d().world_2d
 			subviewport_2D.global_canvas_transform = EditorInterface.get_editor_viewport_2d().global_canvas_transform
 			subviewport_2D.size = EditorInterface.get_editor_viewport_2d().size
-			get_parent().split_offset = (preview_size.x)
+			get_parent().split_offset = (editor_size.x)
 			#Enable 3D subviewport stretch
 			EditorInterface.get_editor_viewport_3d().get_parent().stretch = true
-		Previewer2D3D_Enums.PreviewMode.PREVIEW_3D:
+		DualEditor_Enum.DualEditorMode.MODE_3D:
 			current_subviewport = subviewport_3D
 			subviewport_3D = EditorInterface.get_editor_viewport_3d()
-			get_parent().split_offset = ((get_parent_control().size.x - preview_size.x) - godot_split_size)
+			get_parent().split_offset = ((get_parent_control().size.x - editor_size.x) - godot_split_size)
 			#Disable 3D subviewport stretch
 			EditorInterface.get_editor_viewport_3d().get_parent().stretch = false
 	current_subviewport.render_target_update_mode = SubViewport.UPDATE_ALWAYS
--- addons/DualEditor/Previewer2D3D_Enums.gd
+++
@@ -1,6 +0,0 @@
-class_name Previewer2D3D_Enums
-
-enum PreviewMode {
-	PREVIEW_2D,
-	PREVIEW_3D
-}
